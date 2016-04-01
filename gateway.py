fb5a17f2f8f5d741a27823ff956ba3d57c4771a0
jmartin <jmartin@zikzakmedia.com>
2016-04-01 13:49:11 +0200

    Implement refund method for transactions

--- gateway.py
+++ gateway.py
@@ -19,48 +19,76 @@ class AccountPaymentGatewayTransaction:
         res.append('account.invoice')
         return res
 
-    @classmethod
-    def confirm(cls, transactions):
+    def pay_invoice(self, invoice):
         pool = Pool()
         Invoice = pool.get('account.invoice')
         PayInvoice = pool.get('account.invoice.pay', type='wizard')
+        Invoice.workflow_to_posted([invoice])
+        if not invoice.amount_to_pay:
+            return
+        with Transaction().set_context({'active_id': invoice.id}):
+            session_id, _, _ = PayInvoice.create()
+            pay_invoice = PayInvoice(session_id)
+            pay_invoice.start.currency = self.currency
+            pay_invoice.start.currency_digits = (self
+                .currency_digits)
+            pay_invoice.start.description = self.description
+            pay_invoice.start.journal = self.gateway.journal
+            pay_invoice.start.date = self.date
+            pay_invoice.start.amount = self.amount
+            if invoice.total_amount != self.amount:
+                min_percent_writeoff = invoice.total_amount * (1 -
+                    self.gateway.writeoff_amount_percent)
+                max_percent_writeoff = invoice.total_amount * (1 +
+                    self.gateway.writeoff_amount_percent)
+                [setattr(pay_invoice.ask, f, v)
+                    for f, v in pay_invoice.default_ask(None)
+                        .iteritems()]
 
-        to_update = []
+                if (min_percent_writeoff < self.amount <
+                        max_percent_writeoff
+                        or self.amount > invoice.amount_to_pay):
+                    pay_invoice.ask.type = 'writeoff'
+                    pay_invoice.ask.journal_writeoff = (self
+                        .gateway.journal_writeoff)
+                else:
+                    pay_invoice.ask.type = 'partial'
+            pay_invoice.transition_pay()
+            PayInvoice.delete(session_id)
+
+    @classmethod
+    def pay_invoices(cls, transactions):
         for transaction in transactions:
-            if isinstance(transaction.origin, Invoice):
-                invoice = transaction.origin
-                Invoice.workflow_to_posted([invoice])
-                if not invoice.amount_to_pay:
-                    continue
-                with Transaction().set_context({'active_id': invoice.id}):
-                    session_id, _, _ = PayInvoice.create()
-                    pay_invoice = PayInvoice(session_id)
-                    pay_invoice.start.currency = transaction.currency
-                    pay_invoice.start.currency_digits = (transaction
-                        .currency_digits)
-                    pay_invoice.start.description = transaction.description
-                    pay_invoice.start.journal = transaction.gateway.journal
-                    pay_invoice.start.date = transaction.date
-                    pay_invoice.start.amount = transaction.amount
-                    if invoice.total_amount != transaction.amount:
-                        min_percent_writeoff = invoice.total_amount * (1 -
-                            transaction.gateway.writeoff_amount_percent)
-                        max_percent_writeoff = invoice.total_amount * (1 +
-                            transaction.gateway.writeoff_amount_percent)
-                        [setattr(pay_invoice.ask, f, v)
-                            for f, v in pay_invoice.default_ask(None)
-                                .iteritems()]
+            invoice = transaction.origin
+            transaction.pay_invoice(invoice)
 
-                        if (min_percent_writeoff < transaction.amount <
-                                max_percent_writeoff
-                                or transaction.amount > invoice.amount_to_pay):
-                            pay_invoice.ask.type = 'writeoff'
-                            pay_invoice.ask.journal_writeoff = (transaction
-                                .gateway.journal_writeoff)
-                        else:
-                            pay_invoice.ask.type = 'partial'
-                        pay_invoice.transition_pay()
-                    PayInvoice.delete(session_id)
-                to_update.append(transaction)
+    @classmethod
+    def confirm(cls, transactions):
+        Invoice = Pool().get('account.invoice')
 
-        super(AccountPaymentGatewayTransaction, cls).confirm(to_update)
+        transactions = [t for t in transactions
+            if isinstance(t.origin, Invoice)]
+        cls.pay_invoices(transactions)
+
+        super(AccountPaymentGatewayTransaction, cls).confirm(transactions)
+
+    @classmethod
+    def refund(cls, transactions):
+        Invoice = Pool().get('account.invoice')
+
+        transactions = [t for t in transactions
+            if isinstance(t.origin, Invoice)]
+        cls.pay_invoices(transactions)
+
+        for transaction in transactions:
+            invoice = transaction.origin
+            if invoice.state == 'paid':
+                # TODO: Pending to test if credit invoice is already
+                # created before create a new one
+                credit, = Invoice.credit([invoice])
+
+                # TODO: delete this line when you have developed the refund
+                # transaction through the payment gateway
+                transaction.pay_invoice(credit)
+
+        super(AccountPaymentGatewayTransaction, cls).refund(transactions)
